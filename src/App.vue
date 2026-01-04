25ce20b32d1b062993fe3eaa480014f145ffda6b
RosieOh <dhxogns920@gmail.com>
2026-01-04 22:41:11 +0900

    FEAT: 루트 Vue 컴포넌트 추가

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen bg-amore-cream">
+    <NavBar />
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import NavBar from './components/layout/NavBar.vue'
+</script>
+
