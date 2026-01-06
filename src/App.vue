1564a4ee21dd870f8cb9cd141861ff68fb4b7528
RosieOh <dhxogns920@gmail.com>
2026-01-06 13:18:01 +0900

    FEAT: 페이지 전환 애니메이션 효과 추가

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="min-h-screen bg-amore-cream">
     <NavBar />
-    <router-view />
+    <router-view v-slot="{ Component, route }">
+      <transition
+        :name="route.meta.transition || 'fade'"
+        mode="out-in"
+      >
+        <component :is="Component" :key="route.path" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
